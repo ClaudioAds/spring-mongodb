80fe338f339361dba9585cb60a33babae0989f08
Cláudio Morais <claudio.blz25@gmail.com>
2022-03-24 22:08:46 -0300

    repository with Mongodb connection

--- src/main/java/com/course/springmongodb/resources/UserResources.java
+++ src/main/java/com/course/springmongodb/resources/UserResources.java
@@ -1,6 +1,8 @@
 package com.course.springmongodb.resources;
 
 import com.course.springmongodb.domain.User;
+import com.course.springmongodb.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,12 +16,13 @@ import java.util.List;
 @RequestMapping(value = "/users")
 public class UserResources {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1", "Maria Brown", "maria@gmail.com");
-        User alex = new User("2", "Alex Morais", "alex@gmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, alex));
+
+        List<User> list = userService.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
---
+++ src/main/java/com/course/springmongodb/services/UserService.java
@@ -0,0 +1,19 @@
+package com.course.springmongodb.services;
+
+import com.course.springmongodb.domain.User;
+import com.course.springmongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
