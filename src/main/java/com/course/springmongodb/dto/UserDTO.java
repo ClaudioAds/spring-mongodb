dc79ef493fcc81f78e76cf494f4a118801108f9c
Cláudio Morais <claudio.blz25@gmail.com>
2022-03-28 21:09:16 -0300

    Post method to insert user

--- src/main/java/com/course/springmongodb/dto/UserDTO.java
+++ src/main/java/com/course/springmongodb/dto/UserDTO.java
@@ -5,7 +5,7 @@ import com.course.springmongodb.domain.User;
 import java.io.Serializable;
 
 public class UserDTO implements Serializable {
-    private static final long seriaVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     private String id;
     private String name;
--- src/main/java/com/course/springmongodb/services/UserService.java
+++ src/main/java/com/course/springmongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package com.course.springmongodb.services;
 
 import com.course.springmongodb.domain.User;
+import com.course.springmongodb.dto.UserDTO;
 import com.course.springmongodb.repository.UserRepository;
 import com.course.springmongodb.services.exception.ObjectNotFoundException;
 import org.omg.PortableServer.POAPackage.ObjectAlreadyActive;
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
