59ca841b27231b17220e5a8a237ed640b1d9e77a
Cláudio Morais <claudio.blz25@gmail.com>
2022-04-03 08:56:59 -0300

    Referencing user posts

--- src/main/java/com/course/springmongodb/domain/User.java
+++ src/main/java/com/course/springmongodb/domain/User.java
@@ -1,9 +1,12 @@
 package com.course.springmongodb.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user") //pra dizer que pertence a uma coleção do MongoDB
@@ -14,6 +17,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)//Não vai carregar os posts automaticamente, só quando buscar por posts
+    private List<Post> posts = new ArrayList<>();
 
     public User() {
     }
@@ -30,11 +36,6 @@ public class User implements Serializable {
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
         return id.equals(user.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
     }
 
     public String getId() {
@@ -59,5 +60,18 @@ public class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
