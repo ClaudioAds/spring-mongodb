2c82b02ffccabffb6ed97fd5a11a8f5d984b8565
Cláudio Morais <claudio.blz25@gmail.com>
2022-04-03 11:45:06 -0300

    Adding comments to posts

--- src/main/java/com/course/springmongodb/config/Instantiation.java
+++ src/main/java/com/course/springmongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.course.springmongodb.config;
 import com.course.springmongodb.domain.Post;
 import com.course.springmongodb.domain.User;
 import com.course.springmongodb.dto.AuthorDTO;
+import com.course.springmongodb.dto.CommentDTO;
 import com.course.springmongodb.repository.PostRepository;
 import com.course.springmongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("28/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post1.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
